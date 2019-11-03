bfc5cd062f76c2ce49de8ca755a2cf5b77078373
jvaleskasilva <jessicavaleskas@gmail.com>
2019-11-03 18:00:20 -0300

    feat: incluir classes e metodos

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,12 @@
+class Negociacao {
+  // onde se define os atributos de uma classe
+  constructor(data, quantidade, valor) {
+    this.data = data;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+
+  obtemVolume() {
+    return this.quantidade * this.valor;
+  }
+}
