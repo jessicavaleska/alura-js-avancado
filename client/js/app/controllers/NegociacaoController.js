622920d1f66adbac692a354f661ef2c875eb0a84
jvaleskasilva <jessicavaleskas@gmail.com>
2019-11-20 16:15:02 -0300

    crinado uma instancia da model Negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,18 +3,21 @@ class NegociacaoController {
     // percorre o DOM uma única vez, incluindo os campos como propiedades da classe
     let $ = document.querySelector.bind(document);
 
-    this.inputData = $('#data');
-    this.inputQuantidade = $('#quantidade');
-    this.inputValor = $('#valor');
+    // incluindo a convenção de "privado"
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
   }
 
   adiciona(event) {
     // cancela a submissão do formulários para poder capturar od dados
     event.preventDefault();
 
-    console.log(this.inputData.value);
-    console.log(this.inputQuantidade.value);
-    console.log(this.inputValor.value);
+    let negociacao = new Negociacao(
+      this._inputData.value,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
       
   }
 }
