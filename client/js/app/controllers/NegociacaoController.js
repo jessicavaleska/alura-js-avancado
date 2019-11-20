0590ae79436a9c612916bd15b2fd139508802e04
jvaleskasilva <jessicavaleskas@gmail.com>
2019-11-20 17:06:08 -0300

    incluindo arrow function no projeto

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -16,16 +16,13 @@ class NegociacaoController {
     // transfomar a string '2016-11-12' em new Date(2016, 10, 12)
     let data = new Date(...this._inputData.value
       .split('-')
-      .map(function(item, indice) {
-        // if (indice === 1) {
-        //   return item - 1;
-        // }
-
-        return item - indice % 2;
-      })
+      .map((item, indice) => item - indice % 2)
+      // quando se tem uma única instrução numa arrow function você pode emitir o bloco e retirar o return
     );
 
-    console.log(data);
-      
+    let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+
+    console.log(negociacao);
+    
   }
 }
