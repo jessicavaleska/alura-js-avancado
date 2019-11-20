e5253e5ed6e7ca58a0cbdc853c2e0190e686a7b5
jvaleskasilva <jessicavaleskas@gmail.com>
2019-11-20 15:54:53 -0300

    associando metodos do controller as acoes do usuario

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,16 @@ class NegociacaoController {
     adiciona(event) {
         // cancela a submissão do formulários para poder capturar od dados
         event.preventDefault();
-        alert('Chamei a ação no controller');
+        
+        // mantêm a associação do método querySelector ao document
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
+        
     }
 }
