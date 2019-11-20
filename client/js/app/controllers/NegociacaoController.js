95ce4012a80d654928f9469111c07c881b576154
jvaleskasilva <jessicavaleskas@gmail.com>
2019-11-20 16:06:47 -0300

    evitando percorrer o DOM muitas vezes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,20 @@
 class NegociacaoController {
-    adiciona(event) {
-        // cancela a submissão do formulários para poder capturar od dados
-        event.preventDefault();
-        
-        // mantêm a associação do método querySelector ao document
-        let $ = document.querySelector.bind(document);
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+  constructor() {
+    // percorre o DOM uma única vez, incluindo os campos como propiedades da classe
+    let $ = document.querySelector.bind(document);
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
-        
-    }
+    this.inputData = $('#data');
+    this.inputQuantidade = $('#quantidade');
+    this.inputValor = $('#valor');
+  }
+
+  adiciona(event) {
+    // cancela a submissão do formulários para poder capturar od dados
+    event.preventDefault();
+
+    console.log(this.inputData.value);
+    console.log(this.inputQuantidade.value);
+    console.log(this.inputValor.value);
+      
+  }
 }
